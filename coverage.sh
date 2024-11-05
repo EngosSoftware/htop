1e7a56f44de110a6587e02e1093399ae972df51f
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-11-05 15:11:50 +0100

    Fixed black-box tests.

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 ###############################################################################
-# Dependencies:
-#
-# $ sudo dnf install lcov
-# $ rustup component add llvm-tools-preview
-# $ cargo install grcov
-#
+#                                                                             #
+# Dependencies:                                                               #
+#                                                                             #
+# $ sudo dnf install lcov                                                     #
+# $ rustup component add llvm-tools-preview                                   #
+# $ cargo install grcov                                                       #
+# $ cargo install htop                                                        #
+#                                                                             #
 ###############################################################################
 
 WORKING_DIRECTORY=$(pwd)
@@ -21,7 +23,8 @@ cargo clean
 # set instrumenting variables
 export CARGO_INCREMENTAL=0
 export RUSTDOCFLAGS="-Cpanic=abort"
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTFLAGS="-Cinstrument-coverage -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export LLVM_PROFILE_FILE="$WORKING_DIRECTORY/target/profraw/dsntk-%p-%m.profraw"
 
 # run all unit tests
 cargo +nightly test
@@ -33,7 +36,7 @@ cargo +nightly build
 echo "$BBT_DIRECTORY"
 if [[ -d "$BBT_DIRECTORY" ]]
 then
-  export PATH=$BINARY_PATH:$PATH
+  export PATH="$BINARY_PATH:$PATH"
   cd "$BBT_DIRECTORY" || exit 1
   ./run.sh ./tests
   cd "$WORKING_DIRECTORY" || exit 1
@@ -42,12 +45,33 @@ fi
 # prepare output directories for coverage results
 mkdir ./target/lcov
 mkdir ./target/coverage
+
 # generate coverage info
-grcov . --llvm -s . -t lcov --branch --ignore-not-existing --ignore "*cargo*" --ignore "*tests*" --ignore "*headless_chrome*" -o ./target/lcov/lcov.info
+grcov . \
+      --llvm \
+      -s . \
+      --binary-path "$BINARY_PATH" \
+      -t lcov \
+      --branch \
+      --ignore-not-existing \
+      --ignore "*cargo*" --ignore "*tests*" --ignore "*headless_chrome*" \
+      -o ./target/lcov/lcov.info
+
 # generate coverage report
 genhtml -t "$CARGO_NAME v$CARGO_VERSION" -q -o ./target/coverage ./target/lcov/lcov.info
-# display final message
-echo ""
-echo "Open coverage report: file://$WORKING_DIRECTORY/target/coverage/index.html"
-echo ""
 
+# generate coverage report in PDF format
+if [ "$PDF_REPORT" != "" ]; then
+  echo ""
+  echo "Generating PDF report..."
+  htop -b -l -p A4 --margin=4mm single ./target/coverage/index.html ./target/coverage/coverage.pdf
+fi
+
+# display the final message
+echo ""
+echo "Open coverage report:"
+echo "  HTML file://$WORKING_DIRECTORY/target/coverage/index.html"
+if [ "$PDF_REPORT" != "" ]; then
+  echo "   PDF file://$WORKING_DIRECTORY/target/coverage/coverage.pdf"
+fi
+echo ""
