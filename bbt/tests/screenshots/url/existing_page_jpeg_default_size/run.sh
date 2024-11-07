21d7d91b4675e6db47682006ed40bb9230d283b3
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-11-07 12:45:48 +0100

    Updates.

---
+++ bbt/tests/screenshots/url/existing_page_jpeg_default_size/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#******************************************************************************
+#
+# Take a screenshot of a single HTML page, save the image in JPEG format.
+# The windows size is the default in headless Chrome.
+# The long option name --jpeg is used.
+#
+#******************************************************************************
+
+htop --jpeg url https://www.engos.software actual.1 >> /dev/null 2>&1
